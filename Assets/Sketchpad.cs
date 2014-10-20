f4e805a8735ade570cff81c43ffa20c0b163061b
Ben Wiklund <ben@daisyowl.com>
2014-10-20 15:10:51 -0700

    line drawing

--- Assets/Sketchpad.cs
+++ Assets/Sketchpad.cs
@@ -4,20 +4,42 @@ using System.Collections;
 public class Sketchpad : MonoBehaviour {
 
 	public GameObject particle;
+	public Vector3 lastPoint;
+	public bool isDrawing = false;
 
 	void Start () {
 	
 	}
 
 	void Update () {
-		if (Input.GetMouseButton(0)) {
+		if (Input.GetMouseButton (0)) {
 			int layerMask = 1 << 8; // Maps to CanvasPlane layer
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 			RaycastHit hit;
-			if (Physics.Raycast(ray, out hit, 20F, layerMask)) {
-				Debug.Log (hit.point);
-				Instantiate(particle, hit.point, transform.rotation);
+			if (Physics.Raycast (ray, out hit, 20F, layerMask)) {
+				if (isDrawing) {
+					DrawLine (lastPoint, hit.point);
+				} else {
+					DrawPoint (hit.point);
+				}
+				lastPoint = hit.point;
+				isDrawing = true;
 			}
+		} else {
+			isDrawing = false;
 		}
+	}
+
+	void DrawLine (Vector3 p1, Vector3 p2) {
+		// TODO: use a particle system here, not spheres.
+		float num = 20f; // TODO: use line length, and ignore large gaps in time / space
+		for (int i=0; i<num; i++) {
+			Vector3 p = Vector3.Slerp (p1, p2, i / num);
+			DrawPoint (p);
+		}
+	}
+
+	void DrawPoint (Vector3 p) {
+		Instantiate (particle, p, Quaternion.identity);
 	}
 }
