0817eeebb4b2f4c59055a520e5457bbd87e3c949
Ben Wiklund <ben@daisyowl.com>
2014-10-20 14:11:33 -0700

    drawing test

---
+++ Assets/Sketchpad.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sketchpad : MonoBehaviour {
+
+	public GameObject particle;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		if (Input.GetMouseButton(0)) {
+			int layerMask = 1 << 8; // Maps to CanvasPlane layer
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit, 20F, layerMask)) {
+				Debug.Log (hit.point);
+				Instantiate(particle, hit.point, transform.rotation);
+			}
+		}
+	}
+}
