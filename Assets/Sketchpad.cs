425d715d793ff3fe8c9d8eac0bde4728cd18ad89
Ben Wiklund <ben@daisyowl.com>
2014-10-21 18:02:54 -0700

    refactoring, renaming

--- Assets/Sketchpad.cs
+++ Assets/Sketchpad.cs
@@ -8,40 +8,48 @@ public class Sketchpad : MonoBehaviour {
 
 	Vector3 lastPoint;
 	bool isDrawing = false;
-	List<ParticleSystem.Particle> cloud = new List<ParticleSystem.Particle>();
-	bool needsUpdate = false;
+	List<ParticleSystem.Particle> pointList = new List<ParticleSystem.Particle>();
+	bool particleSystemNeedsUpdate = false;
 	bool togglePlaneVisibility = false;
-
+	
 	void Update () {
+		CheckUserInput ();
+
+		if (particleSystemNeedsUpdate) {
+			UpdateParticles ();
+			particleSystemNeedsUpdate = false;
+		}
+	}
+
+	void CheckUserInput () {
 		if (Input.GetMouseButton (0)) {
-			int layerMask = 1 << 8; // Maps to CanvasPlane layer
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			RaycastHit hit;
-			if (Physics.Raycast (ray, out hit, 20F, layerMask)) {
-				if (isDrawing) {
-					DrawLine (lastPoint, hit.point);
-				} else {
-					DrawPoint (hit.point);
-				}
-				lastPoint = hit.point;
-				isDrawing = true;
-				needsUpdate = true;
-			}
+			ApplyUserInput ();
+			isDrawing = true;
 		} else {
 			isDrawing = false;
 		}
+	}
 
-		if (needsUpdate) {
- 			var cloudArray = cloud.ToArray(); // TODO: definitely not most efficient thing possible
-			particleSystem.SetParticles (cloudArray, cloudArray.Length);
-			needsUpdate = false;
+	void ApplyUserInput () {
+		int layerMask = 1 << 8;
+		// Maps to CanvasPlane layer
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit, 20f, layerMask)) {
+			if (isDrawing) {
+				DrawLine (lastPoint, hit.point);
+			} else {
+				DrawPoint (hit.point);
+			}
+			lastPoint = hit.point;
+			particleSystemNeedsUpdate = true;
 		}
 	}
 
 	void DrawLine (Vector3 p1, Vector3 p2) {
 		float dotsPerWorldSpace = 100f;
 		float num = dotsPerWorldSpace * (p1 - p2).magnitude;
-		for (int i=0; i<num; i++) {
+		for (int i = 0; i < num; i++) {
 			Vector3 p = Vector3.Lerp (p1, p2, i / num);
 			DrawPoint (p);
 		}
@@ -51,21 +59,29 @@ public class Sketchpad : MonoBehaviour {
 		var particle = new ParticleSystem.Particle ();
 		particle.position = p;
 		particle.color = new Color (1f, 1f, 1f, 1f);
-		particle.size = 0.02f * Random.Range(0.8f,1f);
-		cloud.Add (particle);
+		particle.size = 0.02f * Random.Range(0.8f, 1f);
+		pointList.Add (particle);
+	}
+
+	/// <remarks>
+	/// definitely not most efficient thing possible, as
+	/// it streams more and more points every frame. A more robust
+	/// solution here would probably break the particles into chunks,
+	/// or figure out a way to ONLY send new points to Shuriken.
+	/// </remarks>
+	void UpdateParticles () {
+		var asArray = pointList.ToArray ();
+		particleSystem.SetParticles (asArray, asArray.Length);
 	}
 
 	void ClearPoints () {
-		cloud.Clear ();
-		needsUpdate = true;
+		pointList.Clear ();
+		particleSystemNeedsUpdate = true;
 	}
 
 	void OnGUI () {
 		if (GUI.Button(new Rect(10, 10, 100, 50), "Clear")) {
 			ClearPoints();
 		}
-
-//		togglePlaneVisibility = GUI.Toggle (new Rect (10, 150, 100, 100), togglePlaneVisibility, "Plane");
-		planeObject.renderer.enabled = togglePlaneVisibility;
 	}
 }
--- README.md
+++ README.md
@@ -8,11 +8,13 @@ Download from here [public link to Tango unity sdk, once available]
 
 Go to `Assets > Import Package > Custom package` and select the package file. Leave all assets selected and import them. You should now have a `TangoSDK` folder in your project.
 
+(NOTE: At this point, unity might be reporting errors in the tango source code. If so, build the project once `File > Build` and they should go away)
+
 As with all Unity / Tango apps, you'll need to add the prefab at `TangoSDK/Core/Prefabs/Tango` to your scene, and take a look at it's components. We only need pose data for this demo, not depth, so leave everything as it is.
 
 Take a look at the `DrawingRig` object in the root of the scene. All of the drawing logic happens in the `Sketchpad.cs` script, which is attached to a particle system. The "drawing" is actually a regular old ParticleSystem. The only difference is that Looping, Play on awake, Emission, and Shape are all disabled (Leaving Renderer on).
 
-Note that `DrawingRig` has a `TangoController` script on it. This is found in the SDK at `TangoSDK/Examples/Scripts/Controllers/TangoController.cs` and used without modification.
+Note that `DrawingRig` has a `TangoController` script on it. This is found in the SDK at `TangoSDK/Examples/Scripts/Controllers/TangoController.cs` and used without modification. This script is what takes the latest pose (position + rotation) data from the device and applies it to the game object.
 
 The actual drawing logic, in `Sketchpad.cs`, is fairly standard unity code. Here's hos it works:
 
@@ -20,6 +22,8 @@ The actual drawing logic, in `Sketchpad.cs`, is fairly standard unity code. Here
 - Project a ray onto the `InvisibleCanvas` plane that is attached to `DrawingRig`
 - Create a bunch of new points from the last point to the new point
 - Update the ParticleSystem with the latest points
+
+Check out the source of `Sketchpad.cs` for more detail. It's not a ton of code.
 
 ### Todo
 
